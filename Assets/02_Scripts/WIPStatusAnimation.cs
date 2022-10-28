2af5b0d96b6d5273973eb44cce0f0664c441d893
tim <timerakkle@gmail.com>
2022-10-28 14:11:21 -0400

    quick commit

---
+++ Assets/02_Scripts/WIPStatusAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WIPStatusAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private MeshRenderer _Renderer;
+
+    void Start()
+    {
+        _Renderer = transform.GetComponentInChildren<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        SetAlpha((Mathf.Sin(Time.time * 4) + 1) / 2);
+    }
+
+    public void SetAlpha(float a)
+    {
+        var color = _Renderer.sharedMaterial.color;
+        color.a = a;
+        _Renderer.sharedMaterial.SetColor("_Color",color);
+    }
+}
